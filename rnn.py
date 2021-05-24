d5fe82daf6cba67c68561c44d6fe8e15135dd14f
Danish Anwer <danish2562022@gmail.com>
2021-05-24 21:14:01 +0200

    RNN

---
+++ rnn.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+from utility import ALL_LETTERS,N_LETTERS
+from utility import load_data,letter_to_tensor,line_to_tensor,random_training_example
+
+class RNN(nn.Module):
+    # nn.RNN
+    def __init__(self,input_size,hidden_size,output_size):
+        super(RNN,self).__init__()
+
+        self.hidden_size=hidden_size
+        self.i2h=nn.Linear(input_size+hidden_size,hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax=nn.LogSoftmax(dim=1)
+
+    def forward(self,input_tensor,hidden_tensor):
+        combined=torch.cat((input_tensor,hidden_tensor),1)
+
+        hidden=self.i2h(combined)
+        output=self.i2o(combined)
+        output=self.softmax(output)
+        return output,hidden
+
+    def init_hidden(self):
+        return torch.zeros(1,self.hidden_size)
+
+
+category_lines,all_categories=load_data()
+n_categories=len(all_categories)
+print(n_categories)
+
+n_hidden=128
+rnn=RNN(N_LETTERS,n_hidden,n_categories)
+
+input_tensor=letter_to_tensor("A")
+hidden_tensor=rnn.init_hidden()
+output,next_hidden=rnn(input_tensor,hidden_tensor)
+print(output.size())
+print(next_hidden.size())
